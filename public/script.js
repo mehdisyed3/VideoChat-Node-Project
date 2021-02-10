94eaaf8d8358a274930a7268061723ab222be5d2
Syed Mehdi <mehdisyed3@gmail.com>
2021-02-10 14:08:46 -0500

    vidoe now displaying.. styling to be done.

---
+++ public/script.js
@@ -0,0 +1,26 @@
+
+
+const videoGrid = document.getElementById('video_grid')
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+
+let myVideoStream;
+
+// getting user camera and microphone
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  myVideoStream = stream
+  addVideoStream(myVideo, stream)
+})
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+
+  videoGrid.append(video)
+}
