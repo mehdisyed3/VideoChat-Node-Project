c114098cd990ebef329379e008b8739a6774a89d
Syed Mehdi <mehdisyed3@gmail.com>
2021-02-17 16:16:18 -0500

    peer.js installed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { resolveSoa } = require('dns')
 const express = require('express')
+const { ExpressPeerServer } = require('peer')
 const app = express()
 
 const server = require('http').Server(app)
@@ -7,6 +8,9 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 const {v4: uuidv4} = require('uuid')
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+})
 
 app.set('view engine','ejs');
 app.use(express.static('public'))
