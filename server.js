1213f633d21e5a85ec4e125ad2ad55752c4c0f40
Syed <mehdisyed3@gmail.com>
2021-02-20 21:00:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const { ExpressPeerServer } = require('peer')
 const app = express()
-
 const server = require('http').Server(app)
 
 const io = require('socket.io')(server)
