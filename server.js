529e9cebae24971c05fe2e6d6cd61b9de1bae6a1
Syed <mehdisyed3@gmail.com>
2021-02-20 21:00:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express()
 const server = require('http').Server(app)
 
 const io = require('socket.io')(server)
-
 const {v4: uuidv4} = require('uuid')
 const peerServer = ExpressPeerServer(server, {
   debug: true
@@ -12,8 +11,6 @@ const peerServer = ExpressPeerServer(server, {
 
 app.set('view engine','ejs');
 app.use(express.static('public'))
-
-
 app.use('/peerjs', peerServer)
 //routes
 app.get('/',(req,res)=>{
